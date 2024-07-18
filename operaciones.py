82aecd4bc967168623708f672da599e39c117485
07JUANDIEGO <juandiegovargas064@gmail.com>
2024-07-17 21:48:47 -0500

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,14 @@
+print ("hola mundo")
+print("hola soy Juan Diego Vargas, Tengo 18 años")
+
+#a continuacion operaciones matematica
+print(8/2)
+print(12+5)
+print(10-5)
+print(2+3)
+
+
+
+#esto es un comentario
+
+#este es un comentario de varias lineas de codigo
